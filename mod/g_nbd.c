90741dd3a583cc80a90fe6b73921ef349ef642d7
Ryan Moeller <ryan-moeller@att.net>
2025-04-18 22:52:25 -0400

    mod: Handle draining inflight in receiver thread
    We avoid doing a wakeup every time the inflight queue is emptied during
    normal operation by observing the state transition to SOFT_DISCONNECTING
    and checking for inflight bios in the receiver thread.

--- mod/g_nbd.c
+++ mod/g_nbd.c
@@ -217,12 +217,9 @@ nbd_conn_enqueue_inflight(struct nbd_conn *nc, struct bio *bp)
 static void
 nbd_conn_remove_inflight_specific(struct nbd_conn *nc, struct nbd_inflight *ni)
 {
-	bool last;
-
 	G_NBD_LOGREQ(G_NBD_TRACE, ni->ni_bio, "%s", __func__);
 	mtx_lock(&nc->nc_inflight_mtx);
 	TAILQ_REMOVE(&nc->nc_inflight, ni, ni_inflight);
-	last = TAILQ_EMPTY(&nc->nc_inflight);
 	mtx_unlock(&nc->nc_inflight_mtx);
 	if (__predict_false(atomic_load_bool(&nc->nc_softc->sc_flushing))) {
 		switch (ni->ni_bio->bio_cmd) {
@@ -231,11 +228,6 @@ nbd_conn_remove_inflight_specific(struct nbd_conn *nc, struct nbd_inflight *ni)
 			wakeup_one(ni);
 		}
 	}
-	if (__predict_false(last && atomic_load_int(&nc->nc_state)
-	    == NBD_CONN_SOFT_DISCONNECTING))
-		wakeup_one(&nc->nc_inflight);
-	G_NBD_LOGREQ(G_NBD_DEBUG0, ni->ni_bio, "%s last=%s", __func__,
-	    last ? "true" : "false");
 }
 
 static inline void
@@ -597,15 +589,26 @@ nbd_error_to_errno(uint32_t error)
 	}
 }
 
+/* Not in releases yet. */
+#ifndef TAILQ_EMPTY_ATOMIC
+#define TAILQ_EMPTY_ATOMIC(head) \
+	(atomic_load_ptr(&(head)->tqh_first) == NULL)
+#endif
+
 static inline bool
 nbd_conn_recv_ok(struct nbd_conn *nc)
 {
 	struct socket *so = nc->nc_socket;
+	enum nbd_conn_state state = atomic_load_int(&nc->nc_state);
 
-	if (__predict_false(atomic_load_int(&nc->nc_state) ==
-	    NBD_CONN_HARD_DISCONNECTING)) {
+	if (__predict_false(state == NBD_CONN_HARD_DISCONNECTING)) {
 		G_NBD_DEBUG(G_NBD_DEBUG0, "nc_state=%s",
 		    nbd_conn_state_str(nc->nc_state));
+		return (false);
+	}
+	if (__predict_false(state == NBD_CONN_SOFT_DISCONNECTING) &&
+	    TAILQ_EMPTY_ATOMIC(&nc->nc_inflight)) {
+		G_NBD_DEBUG(G_NBD_INFO, "soft disconnected");
 		return (false);
 	}
 	if (__predict_false(so->so_error != 0)) {
@@ -623,7 +626,6 @@ static struct nbd_inflight *
 nbd_conn_remove_inflight(struct nbd_conn *nc, uint64_t cookie)
 {
 	struct nbd_inflight *ni, *ni2;
-	bool last;
 
 	G_NBD_DEBUG(G_NBD_TRACE, "%s cookie=%lu", __func__, cookie);
 	mtx_lock(&nc->nc_inflight_mtx);
@@ -633,7 +635,6 @@ nbd_conn_remove_inflight(struct nbd_conn *nc, uint64_t cookie)
 			break;
 		}
 	}
-	last = TAILQ_EMPTY(&nc->nc_inflight);
 	mtx_unlock(&nc->nc_inflight_mtx);
 	if (__predict_false(ni != NULL &&
 	    atomic_load_bool(&nc->nc_softc->sc_flushing))) {
@@ -643,11 +644,6 @@ nbd_conn_remove_inflight(struct nbd_conn *nc, uint64_t cookie)
 			wakeup_one(ni);
 		}
 	}
-	if (__predict_false(last && atomic_load_int(&nc->nc_state)
-	    == NBD_CONN_SOFT_DISCONNECTING))
-		wakeup_one(&nc->nc_inflight);
-	G_NBD_LOGREQ(G_NBD_DEBUG0, ni->ni_bio, "%s last=%s", __func__,
-	    last ? "true" : "false");
 	return (ni);
 }
 
@@ -914,7 +910,7 @@ nbd_conn_soft_disconnect(struct nbd_conn *nc)
 	G_NBD_DEBUG(G_NBD_TRACE, "%s", __func__);
 	m = nbd_request_mbuf(nc->nc_softc->sc_tls, &req);
 	if (m == NULL)
-		goto done;
+		goto error;
 	memset(req, 0, sizeof(*req));
 	req->magic = htobe32(NBD_REQUEST_MAGIC);
 	req->command = htobe16(NBD_CMD_DISCONNECT);
@@ -925,7 +921,7 @@ nbd_conn_soft_disconnect(struct nbd_conn *nc)
 			SOCK_SENDBUF_UNLOCK(so);
 			G_NBD_DEBUG(G_NBD_INFO, "%s disconnecting", __func__);
 			m_free(m);
-			goto done;
+			goto error;
 		}
 		if (sbspace(&so->so_snd) >= needed)
 			break;
@@ -939,20 +935,11 @@ nbd_conn_soft_disconnect(struct nbd_conn *nc)
 	if (error != 0) {
 		G_NBD_DEBUG(G_NBD_ERROR, "%s sosend failed (%d)", __func__,
 		    error);
-		goto done;
+		goto error;
 	}
 	soshutdown(so, SHUT_WR);
-	while (atomic_load_int(&nc->nc_state) == NBD_CONN_SOFT_DISCONNECTING) {
-		mtx_lock(&nc->nc_inflight_mtx);
-		if (TAILQ_FIRST(&nc->nc_inflight) != NULL) {
-			mtx_sleep(&nc->nc_inflight, &nc->nc_inflight_mtx,
-			    PRIBIO | PDROP, "gnbd:inflight", 0);
-			continue;
-		}
-		mtx_unlock(&nc->nc_inflight_mtx);
-		break;
-	}
-done:
+	return;
+error:
 	atomic_store_int(&nc->nc_state, NBD_CONN_HARD_DISCONNECTING);
 }
 
@@ -1154,7 +1141,6 @@ nbd_conn_sender(void *arg)
 	socantrcvmore(so);
 	cv_signal(&nc->nc_receive_cv);
 	sema_wait(&nc->nc_receiver_done);
-	nbd_conn_drain_inflight(nc);
 	nbd_conn_close(nc);
 	if (g_nbd_remove_conn(sc, nc)) {
 		G_NBD_DEBUG(G_NBD_INFO, "%s last connection closed", __func__);
@@ -1179,11 +1165,16 @@ nbd_conn_receiver(void *arg)
 	sched_prio(curthread, PSOCK); /* XXX: or PRIBIO? */
 	thread_unlock(curthread);
 
-	while (__predict_true(atomic_load_int(&nc->nc_state) !=
-	    NBD_CONN_HARD_DISCONNECTING))
+	while (__predict_true(atomic_load_int(&nc->nc_state) ==
+	    NBD_CONN_CONNECTED))
 		nbd_conn_recv(nc);
+	while (atomic_load_int(&nc->nc_state) == NBD_CONN_SOFT_DISCONNECTING &&
+	    !TAILQ_EMPTY_ATOMIC(&nc->nc_inflight))
+		nbd_conn_recv(nc);
+	atomic_store_int(&nc->nc_state, NBD_CONN_HARD_DISCONNECTING);
 	socantsendmore(so);
 	cv_signal(&nc->nc_send_cv);
+	nbd_conn_drain_inflight(nc);
 	wakeup(&sc->sc_queue);
 	sema_post(&nc->nc_receiver_done);
 	kthread_exit();
@@ -1649,7 +1640,7 @@ g_nbd_ctl_scale(struct gctl_req *req, struct g_class *mp)
 		}
 		mtx_unlock(&sc->sc_conns_mtx);
 		wakeup(&sc->sc_queue);
-		/* The sender threads will take care of the cleanup. */
+		/* The sender/receiver threads will take care of the cleanup. */
 		return;
 	}
 	mtx_unlock(&sc->sc_conns_mtx);
@@ -1680,7 +1671,7 @@ g_nbd_destroy(struct g_nbd_softc *sc)
 		nbd_conn_degrade_state(nc, NBD_CONN_SOFT_DISCONNECTING);
 	mtx_unlock(&sc->sc_conns_mtx);
 	wakeup(&sc->sc_queue);
-	/* The sender threads will take care of the cleanup. */
+	/* The sender/receiver threads will take care of the cleanup. */
 }
 
 static void
